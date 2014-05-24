cd6079aee456af469b3c50ca4a243487b1ce816a
Alex Howington <alex.howington@gmail.com>
2014-05-23 19:10:48 -0600

    Add animations to Bird

--- dist/js/game.js
+++ dist/js/game.js
@@ -33,13 +33,15 @@ Bird.prototype = Object.create(Phaser.Sprite.prototype);
 Bird.prototype.constructor = Bird;
 
 Bird.prototype.update = function() {
-  
-  // write your prefab's specific update code here
-  
+  if(this.angle < 90) {
+    this.angle += 2.5;
+  }
 };
 
 Bird.prototype.flap = function() {
   this.body.velocity.y = -400;
+
+  this.game.add.tween(this).to({angle: -40}, 100).start();
 };
 
 module.exports = Bird;
